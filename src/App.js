bbfe5d83c5ceff96d4fd7850ed51106d6791ca64
sud0x00 <sudhanva0asvl@outlook.com>
2023-03-03 10:25:33 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container-fluid text-center">
+      <header className="row p-5">
+        <div className="col-12">
+          <h1 className="display-3">Welcome to My Landing Page</h1>
+          <p className="lead">This is some example text.</p>
+          <button className="btn btn-lg btn-primary">Get Started</button>
+        </div>
       </header>
     </div>
   );
